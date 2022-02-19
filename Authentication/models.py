0c705af34b872749eeac7a129784974ac6166608
michael-basweti <baswetima@gmail.com>
2022-02-19 13:49:27 +0300

    user models

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from datetime import datetime
+# from utils.register_email import gmail_send_pass_reset
 
-# Create your models here.
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from rest_framework.response import Response
+
+
+class Usertypes(models.Model):
+    usertypeid = models.AutoField(primary_key=True)
+    usertype = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'usertypes'
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=datetime.now)
+    deleted = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(blank=True, null=True)
+    usertype = models.ForeignKey(Usertypes, models.DO_NOTHING, db_column='usertype', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        managed = True
+        db_table = 'Authentication_user'
