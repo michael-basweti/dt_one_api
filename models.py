370575312f73cee8c1af3a95cfb2d20cdb2558e0
michael-basweti <baswetima@gmail.com>
2022-02-20 11:45:11 +0300

    add loans models

--- models.py
+++ models.py
@@ -125,6 +125,56 @@ class DjangoSession(models.Model):
         db_table = 'django_session'
 
 
+class Loans(models.Model):
+    loanid = models.AutoField(primary_key=True)
+    requestedby = models.ForeignKey(AuthenticationUser, models.DO_NOTHING, db_column='requestedby')
+    requestedon = models.DateField()
+    requestedat = models.TimeField()
+    amountrequested = models.DecimalField(max_digits=28, decimal_places=2)
+    amountdispatched = models.DecimalField(max_digits=28, decimal_places=2, blank=True, null=True)
+    amountpaid = models.DecimalField(max_digits=28, decimal_places=2, blank=True, null=True)
+    approved = models.BooleanField()
+    remainingamount = models.DecimalField(max_digits=28, decimal_places=2, blank=True, null=True)
+    approvedby = models.ForeignKey(AuthenticationUser, models.DO_NOTHING, db_column='approvedby', blank=True, null=True)
+    paymentdate = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'loans'
+
+
+class Payavenue(models.Model):
+    payavenueid = models.AutoField(primary_key=True)
+    payavenuedescription = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'payavenue'
+
+
+class Payments(models.Model):
+    paymentid = models.AutoField(primary_key=True)
+    processedby = models.ForeignKey(AuthenticationUser, models.DO_NOTHING, db_column='processedby')
+    processedon = models.DateField()
+    processedat = models.TimeField()
+    amountpaid = models.DecimalField(max_digits=28, decimal_places=2)
+    paymenttype = models.ForeignKey('Paymenttype', models.DO_NOTHING, db_column='paymenttype')
+    payavenue = models.ForeignKey(Payavenue, models.DO_NOTHING, db_column='payavenue')
+
+    class Meta:
+        managed = False
+        db_table = 'payments'
+
+
+class Paymenttype(models.Model):
+    paymenttypeid = models.AutoField(primary_key=True)
+    paymentypedescription = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'paymenttype'
+
+
 class Usertypes(models.Model):
     usertypeid = models.AutoField(primary_key=True)
     usertype = models.CharField(max_length=20)
