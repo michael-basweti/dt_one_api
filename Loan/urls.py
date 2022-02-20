cd55d072a1bf27a3f212db68d86e45b0c18679c3
michael-basweti <baswetima@gmail.com>
2022-02-20 17:49:52 +0300

    deny and approve loan

--- Loan/urls.py
+++ Loan/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('request', views.LoansView.as_view(), name='request'),
+    path('approve', views.ApproveView.as_view(), name='approve'),
+    path('deny', views.DenyLoan.as_view(), name='deny'),
 ]
