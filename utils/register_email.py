e618069052e69263954c5c0d730724aa0cd5c94f
michael-basweti <baswetima@gmail.com>
2022-02-20 17:50:57 +0300

    send emails

--- utils/register_email.py
+++ utils/register_email.py
@@ -27,7 +27,26 @@ def loan_acknowledgement(emailUser):
     recepient = emailUser
     send_mail(subject, plain_message,
          EMAIL_HOST_USER, [recepient], fail_silently = False, html_message=html_message)
+
+
+def loan_approval(emailUser):
+    subject = 'Loan Approval'
+
+    html_message = render_to_string('email/approved.html')
+    # message = 'Visit the site and login with your email: {} and password: {}. You can change yor password after signing in.'.format(emailUser,password)
+    plain_message = strip_tags(html_message)
+    recepient = emailUser
+    send_mail(subject, plain_message,
+         EMAIL_HOST_USER, [recepient], fail_silently = False, html_message=html_message)
          
         
 
+def loan_denied(emailUser):
+    subject = 'Loan Denied'
 
+    html_message = render_to_string('email/denied.html')
+    # message = 'Visit the site and login with your email: {} and password: {}. You can change yor password after signing in.'.format(emailUser,password)
+    plain_message = strip_tags(html_message)
+    recepient = emailUser
+    send_mail(subject, plain_message,
+         EMAIL_HOST_USER, [recepient], fail_silently = False, html_message=html_message)
