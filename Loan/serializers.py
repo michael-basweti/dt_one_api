cab164d9a2570437e6e4da00c4ee78ede5140b90
michael-basweti <baswetima@gmail.com>
2022-02-20 13:30:58 +0300

    loans serializers

--- Loan/serializers.py
+++ Loan/serializers.py
@@ -1,2 +1,25 @@
 from rest_framework import serializers
 from .models import Loans, Payavenue, Paymenttype, Payments
+
+
+class LoansSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loans
+        fields = '__all__'
+
+
+class PayavenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payavenue
+        fields = '__all__'
+
+
+class PaymentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paymenttype
+        fields = '__all__'
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = '__all__'
