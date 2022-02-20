24544bc35509e4d29eb457531fbc11536f90c75e
michael-basweti <baswetima@gmail.com>
2022-02-20 07:18:11 +0300

    add templates

--- dt_one/settings.py
+++ dt_one/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'dt_one.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,9 +124,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
