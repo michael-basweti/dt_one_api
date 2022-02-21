50c9a96dfd5aed2962cb15d5b34c47703723842a
michael-basweti <baswetima@gmail.com>
2022-02-21 23:07:31 +0300

    payavenues url

--- Loan/urls.py
+++ Loan/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('request', views.LoansView.as_view(), name='request'),
     path('approve', views.ApproveView.as_view(), name='approve'),
     path('deny', views.DenyLoan.as_view(), name='deny'),
+    path('payavenues', views.GetPayAvenues.as_view(), name='payavenues'),
 ]
