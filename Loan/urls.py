b478261bbdeae05c79c2100b9d2ee373c26a5f93
michael-basweti <baswetima@gmail.com>
2022-03-09 08:31:11 +0300

    add make payments url

--- Loan/urls.py
+++ Loan/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('request', views.LoansView.as_view(), name='request'),
     path('approve', views.ApproveView.as_view(), name='approve'),
     path('deny', views.DenyLoan.as_view(), name='deny'),
+    path('makepayments', views.CreatePayment.as_view(), name='makepayments'),
     path('payavenues', views.GetPayAvenues.as_view(), name='payavenues'),
     path('unprocessed', views.GetUnProcessedLoans.as_view(), name='unprocessed'),
     path('oneunprocessed/<loanid>', views.GetOneUnProcessedLoans.as_view(), name='oneunprocessed'),
     path('userloans', views.GetUserLoans.as_view(), name='userloans'),
-    path('makepayments', views.CreatePayment.as_view(), name='makepayments'),
 ]
